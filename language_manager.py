9b2a0aec61c2cf6737693c3e34b63907c5018da3
zamzhang <136144125+zamzhang@users.noreply.github.com>
2024-07-18 02:58:05 +0800

    Update language_manager.py

--- language_manager.py
+++ language_manager.py
@@ -1,13 +1,8 @@
-import os
+# 在 language_manager.py 中的 get_translation 函数
 
-class LanguageManager:
-    def __init__(self):
-        self.language = os.getenv('LANGUAGE', 'zh')
+def get_translation(key, language=None):
+    if language is None:
+        language = language_manager.get_language()
 
-    def get_language(self):
-        return self.language
-
-    def set_language(self, language):
-        self.language = language
-
-language_manager = LanguageManager()
+    # 根据指定的语言返回对应的翻译，如果指定语言不存在则默认使用中文
+    return translations.get(language, translations['zh']).get(key, translations['zh'].get(key, key))
